0900d0fced4f987d8e6af57eb7fa1eebb9271f34
Techexplified <tech@explified.com>
2025-11-21 15:47:02 +0530

    updated the index for allowing all the routes as of now

--- index.js
+++ index.js
@@ -10,11 +10,12 @@ require("dotenv").config();
 const app = express();
 
 app.use(cors({
-  origin: process.env.CLIENT_ORIGIN || "http://localhost:5173",
+  origin: "*",
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
+
 
 app.use(cookieParser());
 app.use(express.json());
